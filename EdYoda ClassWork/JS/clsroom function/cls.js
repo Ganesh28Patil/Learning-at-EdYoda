2ef5e71e6ba998c844274d6d8be52de4a85dce77
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-08-12 21:52:54 +0530

    cls

---
+++ EdYoda ClassWork/JS/clsroom function/cls.js
@@ -0,0 +1,31 @@
+/*
+var num = 5; // 6 5 
+num++
+var next = num;
+console.log(next);
+next--;
+console.log(next);
+console.log(!Boolean(next));
+*/
+/*
+var num = 5;
+var next = ++num;
+console.log('next:', next)
+var prev = --num;
+console.log('prev:', prev)
+*/
+/*
+let x = 10;
+
+let y = (4*(x**2)+18-(23*x)/((5/9)*x+18*((x**3)/33))*x) +(x*(15/12));
+console.log(y)
+*/
+/*
+var password = "12345678";
+if(password.length >= 8){
+    console.log("Acceptable");
+}else{
+    console.log("Not Acceptable");
+}
+*/
+
